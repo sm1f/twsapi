3a773366989d8fbcb60b039b74a822fc229fad58
sm1f <stephen_morrisson@yahoo.com>
2017-10-04 09:55:35 -0400

    Added ETWrapper h cpp.

---
+++ Watcher/ETWrapper.cpp
@@ -0,0 +1,4 @@
+// CopyRight Stephen Morrisson 2017
+// All rights reserved.
+
+#include "ETWrapper.h"
---
+++ Watcher/ETWrapper.h
@@ -0,0 +1,32 @@
+
+// CopyRight Stephen Morrisson 2017
+// All rights reserved.
+
+
+// Electronic Trader Wrapper around IB::EWrapper
+
+
+#ifndef __ET_WRAPPER_H__
+#define __ET_WRAPPER_H__
+
+
+//class ET_Wrapper : public IB::EWrapper {
+//};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif // __EB_WRAPPER_H__
+
